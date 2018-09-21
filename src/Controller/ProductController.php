e1391d09990207974069efbf13d5f442d1b16756
Florian <florian.a@live.fr>
2018-09-21 12:09:42 +0200

    basic pages

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,16 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/product/{page}", name="product")
+     * @Route("/product/{productId}", name="product")
      */
-    public function index()
+    
+    public function index(Request $request, $productId = 0)
     {
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'productId' => $productId,
         ]);
     }
 }
